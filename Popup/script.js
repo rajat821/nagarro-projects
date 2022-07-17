3c3fcf137a13415688c308d31684f80158103163
rajat821 <chandrarajatkumar@gmail.com>
2022-07-18 01:18:49 +0530

    initial

---
+++ Popup/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.clicks').on('click',function(){
+        $('#box').addClass('show');
+    });
+
+    $('.submission').on('click',function(){
+        $('#box').removeClass('show');
+    });
+})
